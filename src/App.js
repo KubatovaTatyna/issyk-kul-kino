3012aaf1d5745cca3c8aa7f4973681abe4dd7f1f
KubatovaTatyna <tatynakubatova@gmail.com>
2023-02-13 16:23:02 +0600

    add sessions route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Movies from './components/MoviesPage/Movies';
 import DescriptionPage from './components/DescriptionPage/DescriptionPage';
 import Contact from './components/ContactPage/ContactPage';
+import Sessions from './components/Sessions/Sessions';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} exact />
         <Route path='/cinema' element={<AboutPage />} />
+        <Route path='/sessions' element={<Sessions />} />
         <Route path='/films' element={<Movies />} />
         <Route path='/films/:descriptionId' element={<DescriptionPage />} />
         <Route path='/contacts' element={<Contact />} />
