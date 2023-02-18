da51d70e15803357995a794ab99424c55b040849
KubatovaTatyna <tatynakubatova@gmail.com>
2023-02-18 23:32:32 +0600

    Change footer component and add resposive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,21 @@
 import './Footer.scss';
 import { Link } from 'react-router-dom';
 import logo from '../../images/footerlogo.png'
-import Logo from '../Logo/Logo'
+import Logo from '../Logo/Logo';
+import Navbar from '../Navbar/Navbar';
+
 const Footer = () => {
   return ( <div className='Footer'>
     <div className='row'>
-      <div className='col-9'>
+      <div className='col-md-9 col-12'>
         <div className='row'>
-          <div className='col-2'><Logo /></div>
-          <div className='col-9'>
+          <div className='col-md-4 col-12' ><Logo /></div>
+          <div className='col-md-7 col-12'>
             <Navbar />
           </div>
         </div>
       </div>
-      <div className='col-3 copyright'>COPYRIGHT “ЫСЫК - КОЛ” 2023 </div>
+      <div className='col-md-3 col-12 copyright'>COPYRIGHT “ЫСЫК - КОЛ” 2023 </div>
     </div>
   </div> );
 }
