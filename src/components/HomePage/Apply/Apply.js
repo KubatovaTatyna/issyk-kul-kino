b7bf580a426791de4382dcf2dcc25eff878749ca
KubatovaTatyna <tatynakubatova74@gmail.com>
2023-02-20 11:30:24 +0600

    Styled apply component

--- src/components/HomePage/Apply/Apply.js
+++ src/components/HomePage/Apply/Apply.js
@@ -1,7 +1,10 @@
+import './Apply.scss';
 
 const Apply = () => {
     return ( 
-        <a href='https://forms.gle/bqDXPoDRSiZN3V836'><h3  className="text-center mt-5 mb-5">Отправить заявку на бронь сеанса</h3></a>
+        <a href='https://forms.gle/bqDXPoDRSiZN3V836' className='ApplyHover'>
+            <h3  className="text-center mt-5 mb-5 Apply">Отправить заявку на бронь сеанса</h3>
+        </a>
      );
 }
  
